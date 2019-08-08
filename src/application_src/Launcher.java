35e25d721e2b8852736bc3fd44d53ce63765f163
natepap <47045300+natepap@users.noreply.github.com>
2019-08-08 09:36:43 -0400

    -u

--- src/application_src/Launcher.java
+++ src/application_src/Launcher.java
@@ -4,7 +4,7 @@ package application_src;
 public class Launcher {
 
     public static void main(String[] args){
-        MainApp.main(new String[] {"--module-path=C:\\Users\\njpap\\java tools\\javafx-sdk-12.0.1\\lib",
+        MainApp.main(new String[] {"--module-path=C:\\Users\\njpap\\java tools\\javafx-jmods-12.0.2",
                 "--add-modules=javafx.fxml,javafx.controls,javafx.base,javafx.graphics,javafx.media,javafx.swing,javafx.web",
                 "--add-exports javafx.graphics/com.sun.javafx.utils=ALL-UNNAMED"});
     }
